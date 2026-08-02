3234fe0f28ec314a0a8a4978c3dd106ee6a109dd
dparodi935 <dparodi935@gmail.com>
2026-08-02 12:43:04 +0200

    Added 2D plotting class

--- astrodynamics/visual.py
+++ astrodynamics/visual.py
@@ -27,6 +27,25 @@ def return_orbital_plotting_points(orbital_shape, mu):
     Q = return_perifocal_to_bodycentric_equatorial_matrix(orbital_shape)
     bodycentric_pos_vectors = np.matmul(Q, perifocal_pos_vectors)
 
-
     return bodycentric_pos_vectors
 
+def plot_orbit_shape(orbital_shape, mu):
+    positions = return_orbital_plotting_points(orbital_shape, mu)
+    
+    plt.plot(positions[0],positions[1])
+    
+class Plotter2D():
+    def __init__(self, mu):
+        self.points = []
+        self.lines = []
+        self.mu = mu
+    
+    def add_orbit(self, orbital_shape):
+        position_vectors = return_orbital_plotting_points(orbital_shape, self.mu)
+        plt.plot(position_vectors[0], position_vectors[1])
+        
+    def add_point(self, position):
+        plt.plot(position[0],position[1])
+    
+    def plot(self):
+        plt.show()
