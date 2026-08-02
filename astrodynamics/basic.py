99f0f497631415a8bca84c536f3557f8acebf18a
dparodi935 <dparodi935@gmail.com>
2026-08-02 12:16:57 +0200

    Added calculations for perifocal to bodycentric transformation matrix, and added a function to get the state from the orbital elements

--- astrodynamics/basic.py
+++ astrodynamics/basic.py
@@ -1,7 +1,22 @@
 import numpy as np
 from numpy.linalg import norm
+#orbital_elements = h, i, raan, e, argp, ta
 
-def elements_from_state(mu, r_vector,v_vector):
+def return_perifocal_to_bodycentric_equatorial_matrix(orbital_shape):
+    h, i, raan, e, argp = orbital_shape
+    Q = np.zeros((3,3))
+    
+    s_i, c_i = np.sin(i), np.cos(i)
+    s_raan, c_raan = np.sin(raan), np.cos(raan)
+    s_argp, c_argp = np.sin(argp), np.cos(argp)
+
+    Q[0] = [- s_raan * c_i * s_argp + c_raan * c_argp , c_raan * c_i * s_argp + s_raan * c_argp, s_i * s_argp]
+    Q[1] = [- s_raan * c_i * c_argp - c_raan * s_argp , c_raan * c_i * c_argp - s_raan * s_argp, s_i * c_argp]
+    Q[2] = [s_raan * s_i, - c_raan * s_i, c_i]
+
+    return Q
+
+def elements_from_state(mu, r_vector, v_vector):
     ''' Assumes reference plane is the x-y plane
     '''
     r = norm(r_vector)
@@ -57,3 +72,30 @@ def elements_from_state(mu, r_vector,v_vector):
         ta = 2*np.pi - np.arccos(x)
 
     return h, i, raan, e, argp, ta
+
+def state_from_elements(mu, orbital_elements):
+    h, i, raan, e, argp, ta = orbital_elements
+
+    #calculate perifocal position
+    r_0 = h**2/mu
+    r = r_0/(1+e*np.cos(ta))
+    x = r * np.cos(ta)
+    y = r * np.sin(ta)
+    z = 0
+    pf_position = np.array([x,y,z])
+    
+    #calculate perifocal velocity 
+    vx = - mu * np.sin(ta)/h
+    vy = mu * (e + np.cos(ta))/h
+    vz = 0 
+    pf_velocity = np.array([vx, vy, vz])
+    
+    #perifocal to bodycentric transformation matrix
+    orbital_shape = orbital_elements[:5]
+    Q = return_perifocal_to_bodycentric_equatorial_matrix(orbital_shape)
+    
+    #use matrix to transform
+    position = np.matmul(Q, pf_position)
+    velocity = np.matmul(Q, pf_velocity)
+    
+    return position, velocity
