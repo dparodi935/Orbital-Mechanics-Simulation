94034120d4dcec3cbe530116e3cbfcecf7dffaac
dparodi935 <dparodi935@gmail.com>
2026-09-21 18:41:10 +0100

    Added units and time constants

--- astrodynamics/constants.py
+++ astrodynamics/constants.py
@@ -1,4 +1,9 @@
-G = 6.674e-11
-solar_mass = 1.9884e+30 
-earth_mass = 5.9722e+24
-earth_radius = 6.371e+6
+G = 6.674e-11 # m^3 s^-2 kg^-1
+solar_mass = 1.9884e+30 # kilograms
+earth_mass = 5.9722e+24 # kilograms
+earth_radius = 6.371e+6 # metres
+au = 1.495979e+11 # metres
+
+# time
+day = 24 * 60 * 60 # seconds
+cy = 36525 * day # Julian century in seconds
